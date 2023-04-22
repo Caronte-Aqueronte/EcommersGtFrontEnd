dd0df29797fa244da484c54199dbf931300d5720
Luis Monterroso <namenotfound4004@gmail.com>
2023-04-21 23:32:09 -0600

    Terminada la funcionalidad de eliminar carrito

--- src/app/servicios/carrito.service.ts
+++ src/app/servicios/carrito.service.ts
@@ -9,29 +9,65 @@ import { CookieService } from 'ngx-cookie-service';
 })
 export class CarritoService {
   private url: string = 'http://localhost:3000/carrito'; //url de la api
-  constructor(private http: HttpClient, private cookiesService: CookieService) {}
+  constructor(
+    private http: HttpClient,
+    private cookiesService: CookieService
+  ) {}
 
-  public ingresarProductoACarrito(id: string, usuario:string): Observable<any> {
-    const formData = new FormData()
-    formData.append("id", id);
-    formData.append("usuario", usuario)
+  /**
+   * Envia un formData ha /ingresarProductoACarrito por post para ingresar un articulo al carrito del cliente
+   * @param id
+   * @param usuario
+   * @returns
+   */
+  public ingresarProductoACarrito(
+    id: string,
+    usuario: string
+  ): Observable<any> {
+    const formData = new FormData();
+    formData.append('id', id);
+    formData.append('usuario', usuario);
     return this.http.post<Observable<any>>(
       this.url + '/ingresarProductoACarrito',
       formData
     );
   }
-
-  public mostrarCarritoDeUsuario(): Observable<any>{
-    //mandamos a traer al usuario
-    const usuario = this.cookiesService.get('correoElectronicoEcommers');
-    return this.http.get<Observable<any>>(this.url + `/mostrarCarritoDeUsuario?usuario=${usuario}`);
-  }
-
+  /**
+   * envia un post hacie /borrarDelCarrito de la api para borrar el articulo del carrito
+   * @param articulo
+   * @returns
+   */
   public borrarDelCarrito(articulo: any): Observable<any> {
-
     return this.http.post<Observable<any>>(
       this.url + '/borrarDelCarrito',
       articulo
+    );
+  }
+
+  /**
+   * envia un post hacie /borraTodoElCarrito de la api para borrar todo el carrito de un usuario
+   * @param articulo
+   * @returns
+   */
+  public borraTodoElCarrito(): Observable<any> {
+    //mandamos a traer al usuario
+    const usuarioCookies = this.cookiesService.get('correoElectronicoEcommers');
+    const usuario  = new Object({usuario: usuarioCookies});
+    return this.http.post<Observable<any>>(
+      this.url + '/borraTodoElCarrito',
+      usuario
+    );
+  }
+
+  /**
+   * envia un get ha /mostrarCarritoDeUsuario para obtener el carrito de un usuario
+   * @returns
+   */
+  public mostrarCarritoDeUsuario(): Observable<any> {
+    //mandamos a traer al usuario
+    const usuario = this.cookiesService.get('correoElectronicoEcommers');
+    return this.http.get<Observable<any>>(
+      this.url + `/mostrarCarritoDeUsuario?usuario=${usuario}`
     );
   }
 }
--- src/app/tu-carrito/tu-carrito.component.ts
+++ src/app/tu-carrito/tu-carrito.component.ts
@@ -1,5 +1,10 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, Inject, OnInit } from '@angular/core';
 import { CarritoService } from '../servicios/carrito.service';
+import {
+  MAT_DIALOG_DATA,
+  MatDialog,
+  MatDialogRef,
+} from '@angular/material/dialog';
 
 @Component({
   selector: 'app-tu-carrito',
@@ -8,22 +13,70 @@ import { CarritoService } from '../servicios/carrito.service';
 })
 export class TuCarritoComponent implements OnInit {
   carrito: Array<any>;
-  constructor(private carritoService: CarritoService) {
+  constructor(
+    private carritoService: CarritoService,
+    private dialog: MatDialog
+  ) {
     this.carrito = [];
     this.mostrarArticulos();
   }
   ngOnInit(): void {}
 
+  /**
+   * Manda a llamar a todos los articulos de un carrito a la api
+   */
   private mostrarArticulos(): void {
     this.carritoService.mostrarCarritoDeUsuario().subscribe((respuesta) => {
       this.carrito = respuesta;
-      console.log(this.carrito);
     });
   }
 
+  /**
+   * Detecta cuando un articulo ha sido borrado del carrito y refresca los componentes
+   * @param event
+   */
   public mostrarDialog(event: any) {
     if (event == true) {
+      //si se ejecuta el evento y la bandera es true entonces cargamos los componentes de nuevo
       this.mostrarArticulos();
     }
   }
+
+  public borrarTodoElCarrito(): void {
+    this.carritoService.borraTodoElCarrito().subscribe((respuesta) => {
+      if (respuesta == null) {
+        this.openDialog("Parametros vacios.", false);
+      }
+      if(respuesta.respuesta == true){
+        this.openDialog("Se elimino todo el carrito con extito.", true);
+        this.mostrarArticulos();
+      }else{
+        this.openDialog("No se elimino el carrito.", false);
+      }
+    });
+  }
+
+  public openDialog(mensaje: string, acierto: boolean): void {
+    this.dialog.open(DialogCarrito, {
+      data: { mensaje: mensaje, acierto: acierto },
+    });
+  }
+}
+
+//Clases que son las encargadas de mostrar un dialog
+export interface DialogData {
+  mensaje: string;
+  acierto: boolean;
+}
+
+@Component({
+  selector: 'dialog-elements-example-dialog',
+  templateUrl: './dialog-elements-example-dialog.html',
+  styleUrls: ['dialog-elements-example-dialog.css'],
+})
+export class DialogCarrito {
+  constructor(
+    public dialogRef: MatDialogRef<DialogCarrito>,
+    @Inject(MAT_DIALOG_DATA) public data: DialogData
+  ) {}
 }
